a1fbd047fe2a8371865ba33b416aaa4baea1554e
ibou-dia <ibrahimadia800@gmail.com>
2025-05-13 08:01:58 +0000

    modification event done

--- event_manager/urls.py
+++ event_manager/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('payment/<str:event_id>/', views.pay_view, name='payment'),
 
     path('event/create/', views.create_event, name='create_event'),
+    path('event/<str:event_id>/edit/', views.edit_event, name='edit_event'),
     path('event/<str:event_id>/', views.event_detail, name='event_detail'),
     path('event/<str:event_id>/register/', views.register_for_event, name='register_for_event'),
     path('event/<str:event_id>/comment/', views.add_comment, name='add_comment'),
